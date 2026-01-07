ae3caeba1f8ad9d09a971e767aecf2d550b1a625
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-01-06 22:24:11 -0500

    ch 5

--- learncpp/main.cpp
+++ learncpp/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "test.hpp"
+#include <string>
+#include <string_view>
 
 int f();
 
@@ -13,7 +15,12 @@ int main(){
     // // add(3, 4);
     // f();
 
-    int x { 2'147'483'647 };
-    std::cout << x + 1 << "\n"; //overflow, -2147483648
+    // int x { 2'147'483'647 };
+    // std::cout << x + 1 << "\n"; //overflow, -2147483648
+    std::string_view sv { "hello "};
+    std::string s { sv };
+
+    std::cout << s << "\n";
+
     return 0;
 }
